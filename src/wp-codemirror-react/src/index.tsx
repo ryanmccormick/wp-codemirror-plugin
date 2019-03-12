9ad39dd2d6ae960278f04642f7e37398960b6db9
Ryan McCormick <ryan.mccormick@rtmccormick.com>
2019-03-12 01:34:14 -0500

    more cleanup

--- src/wp-codemirror-react/src/index.tsx
+++ src/wp-codemirror-react/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
 import { WpCodeMirror } from './wp-codemirror/WpCodeMirror';
 import 'codemirror/lib/codemirror.css';
 import './index.css';
@@ -10,26 +9,10 @@ const elements = Array.from(document.querySelectorAll("pre[lang]")) as Array<HTM
 
 if (elements.length > 0) {
   elements.forEach((element: HTMLPreElement) => {
-    const parentElement = element.parentElement!;
-    const newHostElement = document.createElement('pre');
     const lang = (element.getAttribute('lang') || '').toLowerCase();
     const line = parseInt((element.getAttribute('line') || '0'), 10);
     const codeBlock = element.innerText;
 
-    //    insertBefore<T extends Node>(newChild: T, refChild: Node | null): T;
-    parentElement.insertBefore(newHostElement, element);
-    parentElement.removeChild(element);
-
-
-    ReactDOM.render(<WpCodeMirror codeBlock={codeBlock} lang={lang} line={line}/>, newHostElement);
+    ReactDOM.render(<WpCodeMirror codeBlock={codeBlock} lang={lang} line={line}/>, element);
   });
 }
-
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
